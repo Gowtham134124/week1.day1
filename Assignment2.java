3a09cd75f98c5d4363a56e19b3d3d98bccacb26d
Gowtham134124 <93467695+Gowtham134124@users.noreply.github.com>
2021-10-31 14:11:13 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,36 @@
+package assignments;
+
+public class Assignment2 {
+	// Check if the number is positive or negative
+	public static void main(String[] args) {
+		int num = 50;
+		int numTwo = -1;
+		int numThree = 1;
+		int numFour = 0;
+		
+		
+		
+		if (num * numThree == 50) {
+			System.out.println("50 is positive");
+
+		}
+		if (num * numTwo > 0) {
+			System.out.println(" 50  is negative");
+
+		} 
+		
+		else if (num ==numFour) {
+		
+	
+			
+			System.out.println("Neither negative nor positive");
+			
+			
+			
+			
+		}
+	
+
+	}
+
+}
---
+++ AssignmentPractice.java
@@ -0,0 +1,26 @@
+package assignments;
+
+public class AssignmentPractice {
+
+	public static void main(String[] args) {
+		
+		//convert negative to positive
+		
+		int number = -10;
+		number=(number)*(-1);
+		System.out.println(number);
+		
+		if (number!= -0 || (number == 0 )) {
+			System.out.println("number is converted");
+		}
+			else {
+				System.out.println("number is not converted");
+			}
+			
+			
+			
+		}
+
+	}
+
+
